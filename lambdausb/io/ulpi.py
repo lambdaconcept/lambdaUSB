8a2d9ccefa71a0cf621721eae15ab9c292998db4
Jean-François Nguyen <jf@lambdaconcept.com>
2020-05-28 19:08:26 +0200

    io.ulpi: clean up and comment the PHY FSM.

--- lambdausb/io/ulpi.py
+++ lambdausb/io/ulpi.py
@@ -24,22 +24,22 @@ class PHY(Elaboratable):
 
         self.rx_depth = rx_depth
         self.tx_depth = tx_depth
-        self._pins    = pins
+        self._pins = pins
 
     def elaborate(self, platform):
         m = Module()
 
         m.domains += ClockDomain("ulpi", local=True)
 
-        rx_fifo = stream.AsyncFIFO([("data", 8)], self.rx_depth, w_domain="ulpi", r_domain="sync")
-        tx_fifo = stream.AsyncFIFO([("data", 8)], self.tx_depth, w_domain="sync", r_domain="ulpi")
-        m.submodules.rx_fifo = rx_fifo
-        m.submodules.tx_fifo = tx_fifo
-
         m.submodules.xcvr = xcvr = Transceiver(domain="ulpi", pins=self._pins)
         m.submodules.timer = timer = _Timer()
         m.submodules.splitter = splitter = _Splitter()
         m.submodules.sender = sender = _Sender()
+
+        rx_fifo = stream.AsyncFIFO([("data", 8)], self.rx_depth, w_domain="ulpi", r_domain="sync")
+        tx_fifo = stream.AsyncFIFO([("data", 8)], self.tx_depth, w_domain="sync", r_domain="ulpi")
+        m.submodules.rx_fifo = rx_fifo
+        m.submodules.tx_fifo = tx_fifo
 
         m.d.comb += [
             self.rx.stb.eq(rx_fifo.source.valid),
@@ -56,83 +56,108 @@ class PHY(Elaboratable):
             tx_fifo.source.connect(sender.sink),
         ]
 
-        line_state = Signal(2)
+        rx_cmd = Record([
+            ("line_state", 2),
+            ("vbus_state", 2),
+            ("event",      2),
+            ("id",         1),
+            ("alt_int",    1),
+        ])
+
         with m.If(xcvr.source.valid & xcvr.source.cmd):
-            m.d.ulpi += line_state.eq(xcvr.source.data[:2])
+            m.d.ulpi += rx_cmd.eq(xcvr.source.data)
 
         hs_mode = Signal()
 
         with m.FSM(domain="ulpi") as fsm:
+            with m.State("RESET-0"):
+                m.d.comb += xcvr.rst.eq(1)
+                m.d.comb += timer.cnt25ns.eq(1)
+                with m.If(timer.done):
+                    m.next = "RESET-1"
+
+            with m.State("RESET-1"):
+                m.d.comb += timer.cnt25ns.eq(1)
+                with m.If(timer.done):
+                    m.next = "INIT-0"
+
             with m.State("INIT-0"):
-                m.d.ulpi += hs_mode.eq(0)
-                m.d.comb += [
-                    xcvr.rst.eq(1),
-                    timer.cnt10ms.eq(1)
-                ]
-                with m.If(timer.done):
-                    m.next = "INIT-1"
-
-            with m.State("INIT-1"):
-                m.d.comb += timer.cnt10ms.eq(1)
-                with m.If(timer.done):
-                    m.next = "RESET-PHY-1"
-
-            with m.State("RESET-PHY-1"):
+                # Write Function Control register.
                 m.d.comb += [
                     xcvr.sink.data.eq(0x80 | 0x4),
                     xcvr.sink.valid.eq(1)
                 ]
                 with m.If(xcvr.sink.ready):
-                    m.next = "RESET-PHY-2"
-                m.d.comb += timer.cnt5s.eq(1)
-                with m.If(timer.done):
-                    m.next = "INIT-0"
+                    m.next = "INIT-1"
 
-            with m.State("RESET-PHY-2"):
+            with m.State("INIT-1"):
+                # XcvrSelect = 01 (Enable FS transceiver)
+                # TermSelect =  1 (Peripheral FS)
+                # OpMode     = 00 (Normal operation)
+                # Reset      =  1
+                # SuspendM   =  1 (Powered)
+                # Reserved   =  0
                 m.d.comb += [
                     xcvr.sink.data.eq(0b01100101),
                     xcvr.sink.last.eq(1),
                     xcvr.sink.valid.eq(1),
                 ]
-                with m.If(xcvr.source.valid):
-                    m.next = "WRITE-OTGCTRL-1"
-                with m.Elif(timer.done):
-                    m.next = "INIT-1"
+                m.d.comb += timer.cnt25ns.eq(1)
+                with m.If(timer.done):
+                    m.next = "INIT-0"
+                with m.If(xcvr.sink.ready):
+                    m.next = "INIT-2"
 
-            with m.State("WRITE-OTGCTRL-1"):
+            with m.State("INIT-2"):
+                with m.If(xcvr.source.valid & xcvr.source.cmd):
+                    m.next = "INIT-3"
+
+            with m.State("INIT-3"):
+                m.d.comb += timer.cnt2ns.eq(1)
+                with m.If(timer.done):
+                    m.next = "INIT-4"
+
+            with m.State("INIT-4"):
+                # Write OTG Control register.
                 m.d.comb += [
                     xcvr.sink.data.eq(0x80 | 0xa),
                     xcvr.sink.valid.eq(1)
                 ]
                 with m.If(xcvr.sink.ready):
-                    m.next = "WRITE-OTGCTRL-2"
+                    m.next = "INIT-5"
 
-            with m.State("WRITE-OTGCTRL-2"):
+            with m.State("INIT-5"):
+                # DpPullDown = 0 (Disable D+ pull-down resistor)
+                # DmPullDown = 0 (Disable D- pull-down resistor)
                 m.d.comb += [
-                    xcvr.sink.data.eq(0),
+                    xcvr.sink.data.eq(0b00000000),
                     xcvr.sink.last.eq(1),
                     xcvr.sink.valid.eq(1)
                 ]
+                m.d.comb += timer.cnt25ns.eq(1)
+                with m.If(timer.done):
+                    m.next = "INIT-4"
                 with m.If(xcvr.sink.ready):
                     m.next = "IDLE"
-                m.d.comb += timer.cnt50ns.eq(1)
-                with m.If(timer.done):
-                    m.next = "WRITE-OTGCTRL-1"
 
             with m.State("IDLE"):
-                with m.If(line_state == 0):
+                m.d.comb += [
+                    xcvr.source.connect(splitter.sink),
+                    sender.source.connect(xcvr.sink),
+                ]
+                with m.If(rx_cmd.line_state == 0b00):
                     m.d.comb += timer.cnt2ms.eq(1)
                 with m.If(timer.done):
+                    # A reset was requested from the host. If the link is Full Speed, we attempt
+                    # a chirp handshake to advertise High Speed capability.
                     with m.If(hs_mode):
+                        m.d.ulpi += hs_mode.eq(0)
                         m.next = "INIT-0"
                     with m.Else():
                         m.next = "CHIRP-0"
-                m.d.comb += [
-                    xcvr.source.connect(splitter.sink),
-                    sender.source.connect(xcvr.sink)
-                ]
 
             with m.State("CHIRP-0"):
+                # Write Function Control register.
                 m.d.comb += [
                     xcvr.sink.data.eq(0x80 | 0x4),
                     xcvr.sink.valid.eq(1)
@@ -141,8 +166,14 @@ class PHY(Elaboratable):
                     m.next = "CHIRP-1"
 
             with m.State("CHIRP-1"):
+                # XcvrSelect = 00 (Enable HS transceiver)
+                # TermSelect =  1 (Peripheral Chirp)
+                # OpMode     = 10 (Disable bit-stuff and NRZI encoding)
+                # Reset      =  0
+                # SuspendM   =  1 (Powered)
+                # Reserved   =  0
                 m.d.comb += [
-                    xcvr.sink.data.eq(0b01010000),
+                    xcvr.sink.data.eq(0b01010100),
                     xcvr.sink.last.eq(1),
                     xcvr.sink.valid.eq(1)
                 ]
@@ -150,6 +181,7 @@ class PHY(Elaboratable):
                     m.next = "CHIRP-2"
 
             with m.State("CHIRP-2"):
+                # Send a NOPID TXCMD.
                 m.d.comb += [
                     xcvr.sink.valid.eq(1),
                     xcvr.sink.data.eq(0b01000000)
@@ -158,6 +190,7 @@ class PHY(Elaboratable):
                     m.next = "CHIRP-3"
 
             with m.State("CHIRP-3"):
+                # Send a K chirp for 2ms.
                 m.d.comb += [
                     xcvr.sink.valid.eq(1),
                     xcvr.sink.data.eq(0x00),
@@ -168,11 +201,14 @@ class PHY(Elaboratable):
                     m.d.comb += xcvr.sink.last.eq(1)
 
             with m.State("CHIRP-4"):
+                # Host responds by alternating K and J chirps.
+                # FIXME: Detect a K-J-K-J-K-J pattern before going further.
                 m.d.comb += timer.cnt2ms.eq(1)
                 with m.If(timer.done):
                     m.next = "CHIRP-5"
 
             with m.State("CHIRP-5"):
+                # Write Function Control register.
                 m.d.comb += [
                     xcvr.sink.data.eq(0x80 | 0x4),
                     xcvr.sink.valid.eq(1)
@@ -181,6 +217,12 @@ class PHY(Elaboratable):
                     m.next = "CHIRP-6"
 
             with m.State("CHIRP-6"):
+                # XcvrSelect = 00 (Enable HS transceiver)
+                # TermSelect =  0 (Peripheral HS)
+                # OpMode     = 00 (Normal mode)
+                # Reset      =  0
+                # SuspendM   =  1 (Powered)
+                # Reserved   =  0
                 m.d.comb += [
                     xcvr.sink.data.eq(0b01000000),
                     xcvr.sink.last.eq(1),
@@ -255,10 +297,9 @@ class Transceiver(Elaboratable):
 
 class _Timer(Elaboratable):
     def __init__(self):
-        self.cnt50ns = Signal()
+        self.cnt2ns  = Signal()
+        self.cnt25ns = Signal()
         self.cnt2ms  = Signal()
-        self.cnt10ms = Signal()
-        self.cnt5s   = Signal()
         self.done    = Signal()
 
     def elaborate(self, platform):
@@ -266,7 +307,7 @@ class _Timer(Elaboratable):
 
         counter = Signal(30)
 
-        with m.If(self.cnt50ns | self.cnt2ms | self.cnt10ms | self.cnt5s):
+        with m.If(self.cnt2ns | self.cnt25ns | self.cnt2ms):
             with m.If(self.done):
                 m.d.ulpi += counter.eq(0)
             with m.Else():
@@ -274,13 +315,11 @@ class _Timer(Elaboratable):
         with m.Else():
             m.d.ulpi += counter.eq(0)
 
-        with m.If(self.cnt50ns & (counter == 256)):
+        with m.If(self.cnt2ns & (counter == 10)):
             m.d.comb += self.done.eq(1)
-        with m.Elif(self.cnt2ms & (counter == 120000)):
+        with m.If(self.cnt25ns & (counter == 120)):
             m.d.comb += self.done.eq(1)
-        with m.Elif(self.cnt10ms & (counter == 600000)):
-            m.d.comb += self.done.eq(1)
-        with m.Elif(self.cnt5s & (counter == 2**28)):
+        with m.If(self.cnt2ms & (counter == 120000)):
             m.d.comb += self.done.eq(1)
 
         return m
